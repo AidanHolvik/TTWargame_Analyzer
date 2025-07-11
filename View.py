d37d12eb6eeb1691b3eca5939f4786c92a41fa8b
Aidan Holvik <Aidan.Holvik@gmail.com>
2025-07-11 12:43:22 -0400

    Basic functionality for managing units is in place. Next step is to allow user to manage models assigned to each unit.

--- View.py
+++ View.py
@@ -16,51 +16,75 @@ class UnitList(ttk.Frame):
         super().__init__(master, **kw)
         self.model = model
 
+        self.unitNames = []
         self.unitList = tk.Variable(self, [], 'unitNames')
         self.refreshUnitList()
+        self.selectedUnit = ''
+
+        self.rootFrame = ttk.Frame(self)
+        self.unitEditor = UnitEditor(self, model)
 
         # Listbox on left
-        listFrame = ttk.Frame(self)
-        listBox = tk.Listbox(listFrame, listvariable=self.unitList)
-        scrollbar = ttk.Scrollbar(listFrame, orient=tk.VERTICAL, command=listBox.yview)
-        listBox['yscrollcommand'] = scrollbar.set
+        listFrame = ttk.Frame(self.rootFrame)
+        self.listBox = tk.Listbox(listFrame, listvariable=self.unitList)
+        scrollbar = ttk.Scrollbar(listFrame, orient=tk.VERTICAL, command=self.listBox.yview)
+        self.listBox['yscrollcommand'] = scrollbar.set
 
-        listBox.pack(side=tk.LEFT)
+        self.listBox.pack(side=tk.LEFT)
         scrollbar.pack(side=tk.LEFT)
 
         # buttons frame on right
-        buttonFrame = ttk.Frame(self)
+        buttonFrame = ttk.Frame(self.rootFrame)
         self.btnNew = ttk.Button(buttonFrame, text='New', command=self.btnCmdNew)
-        self.btnEdit = ttk.Button(buttonFrame, text='Edit')
-        self.btnDelete = ttk.Button(buttonFrame, text='Delete')
+        self.btnEdit = ttk.Button(buttonFrame, text='Edit', command=self.btnCmdEdit)
+        self.btnDelete = ttk.Button(buttonFrame, text='Delete', command=self.btnCmdDelete)
 
         self.btnNew.pack()
         self.btnEdit.pack()
         self.btnDelete.pack()
+
         
         listFrame.pack(side=tk.LEFT)
         buttonFrame.pack(side=tk.LEFT)
+
+        # self.unitEditor.grid(column=0, row=0, padx=5, pady=5, sticky='nsew')
+        self.rootFrame.grid(column=0, row=0)
+        
+
+        
     
     def refreshUnitList(self):
-        unitNames = []
+        self.unitNames = []
         if self.model is not None:
             for unit in self.model.listUnits():
-                unitNames.append(unit.name)
-        self.unitList.set(unitNames)
+                self.unitNames.append(unit.name)
+        self.unitList.set(self.unitNames)
+    
     
 
     def btnCmdNew(self):
-        # hide all current frames
         # populate unit editor
-        # pack UnitEditor frame
-        pass
-    # TODO: button editUnit
-    # TODO: button deleteUnit
+        self.unitEditor.populate()
+        # Bring unit editor frame to front
+        self.rootFrame.grid_forget()
+        self.unitEditor.grid(column=0, row=0)
+    
+    def btnCmdEdit(self):
+        # populate unit editor
+        self.unitEditor.populate(self.unitNames[self.listBox.curselection()[0]])
+        # Bring unit editor frame to front
+        self.rootFrame.grid_forget()
+        self.unitEditor.grid(column=0, row=0)
+    
+    def btnCmdDelete(self):
+        # TODO: delete assigned models
+        self.model.deleteUnit(self.unitNames[self.listBox.curselection()[0]])
+        self.refreshUnitList()
 
 
     
 class UnitEditor(ttk.Frame):
-    def __init__(self, master, model: Database = None, **kw):
+    def __init__(self, master: UnitList, model: Database = None, **kw):
         super().__init__(master, **kw)
         self.model = model
         self.prevName = None
@@ -86,8 +110,8 @@ class UnitEditor(ttk.Frame):
 
         # Buttons
         buttonFrame = ttk.Frame(self)
-        cancelButton = ttk.Button(buttonFrame, text='Cancel')
-        saveButton = ttk.Button(buttonFrame, text='Save', command=self.saveUnit)
+        cancelButton = ttk.Button(buttonFrame, text='Cancel', command=self.btnCmdCancel)
+        saveButton = ttk.Button(buttonFrame, text='Save', command=self.btnCmdSave)
         cancelButton.pack(side=tk.LEFT)
         saveButton.pack(side=tk.RIGHT)
 
@@ -96,6 +120,7 @@ class UnitEditor(ttk.Frame):
         costFrame.pack()
         # TODO: pack models frame
         buttonFrame.pack()
+
     
     def populate(self, name: str = None):
         self.prevName = name
@@ -107,9 +132,10 @@ class UnitEditor(ttk.Frame):
             self.name.set(unit.name)
             self.cost.set(unit.cost)
     
-    def saveUnit(self):
+    def btnCmdSave(self):
         unit = Unit(self.name.get(), self.cost.get())
-        if unit.name == None:
+        print(unit)
+        if unit.name == None or unit.name.strip() == '':
             return
 
         if unit not in self.model.listUnits():
@@ -122,13 +148,12 @@ class UnitEditor(ttk.Frame):
         elif unit.name == self.prevName:
             self.model.updateUnit(self.prevName, unit)
             # TODO: assign/unassign models to the unit
+        
+        self.master.refreshUnitList()
     
-    def hide(self):
-        # TODO: pack_forget() self
-        pass
-
-    def show(self):
-        pass
+    def btnCmdCancel(self):
+        self.grid_forget()
+        self.master.rootFrame.grid(column=0, row=0)
         
 
 
